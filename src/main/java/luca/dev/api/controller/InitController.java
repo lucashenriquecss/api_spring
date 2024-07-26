044a82e08b50943ad0f6ae52911bf15517f91774
Lucas Henrique <43302698+lucashenriquecss@users.noreply.github.com>
2024-07-25 21:34:29 -0300

    Init project

---
+++ src/main/java/luca/dev/api/controller/InitController.java
@@ -0,0 +1,16 @@
+package luca.dev.api.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class InitController {
+
+    @GetMapping
+    public String init(){
+        return "Init api";
+    }
+}
